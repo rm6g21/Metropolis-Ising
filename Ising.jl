d726df0bcf1454ce3cf521eb2c44f3c7632874ad
rm6g21 <148370853+rm6g21@users.noreply.github.com>
2024-08-27 23:57:41 +0100

    Update Ising.jl

--- Ising.jl
+++ Ising.jl
@@ -1,3 +1,5 @@
+
+
 using Random
 
 
@@ -7,8 +9,8 @@ grid_randoms = rand(Float64,(grid_size, grid_size))
 
 lattice = zeros(Int,grid_size,grid_size)
 
-lattice[grid_randoms .> 0.5] .= 1
-lattice[grid_randoms .<= 0.5] .= -1
+lattice[grid_randoms .>= 0.25] .= 1
+lattice[grid_randoms .< 0.25] .= -1
 
 
 
@@ -32,7 +34,69 @@ function relative_energy(lattice)
     return -energy
     end
 
-print(relative_energy(lattice))
+function metropolis(lattice, temperature,iterations,energy)
+    lattice = copy(lattice)
+    num_spins = zeros(iterations-1)
+    energy_list = zeros(iterations-1)
+
+    for t in 1:iterations-1
+        i = rand(1:grid_size)
+        j = rand(1:grid_size)
+
+        spin_old = lattice[i, j]
+        spin_new = -spin_old
+
+        E_old = 0
+        E_new = 0
+
+        if i > 1
+            E_old += lattice[i - 1, j]
+            E_new += spin_new * lattice[i - 1, j]
+        end
+
+        if i < grid_size
+            E_old += lattice[i + 1, j]
+            E_new += spin_new * lattice[i + 1, j]
+        end
+
+        if j > 1
+            E_old += lattice[i, j - 1]
+            E_new += spin_new * lattice[i, j - 1]
+        end
+
+        if j < grid_size
+            E_old += lattice[i, j + 1]
+            E_new += spin_new * lattice[i, j + 1]
+        end
+
+
+
+        delta_E = E_new - E_old
+
+        if delta_E > 0 && rand() < exp(-delta_E / temperature)
+            lattice[i, j] = spin_new
+            energy += delta_E
+        
+        elseif delta_E <= 0
+            lattice[i, j] = spin_new
+            energy += delta_E
+        end
+
+        num_spins[t] = sum(lattice)
+        energy_list[t] = energy
+    end
+
+    return num_spins, energy_list
+    end
+
+spins, energies = metropolis(lattice, 1.0, 100000, relative_energy(lattice))
+
+using Plots
+#plotlyjs()
+
+
+display(plot(1:99999, spins./(grid_size^2), label="Energy", xlabel="Iteration", ylabel="Energy"))
+
 
 
 
